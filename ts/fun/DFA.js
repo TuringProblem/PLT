18d5cf64eacd3b12474581fe0d481aa0f55812a9
Override <122590935+TuringProblem@users.noreply.github.com>
2026-05-17 14:51:19 -0400

    chore: cleaned up dfa, starting to understand more - made one language

--- ts/fun/DFA.js
+++ ts/fun/DFA.js
@@ -1,36 +1,59 @@
 "use strict";
-const zero = s => {
-    switch (s) {
-        case "q0": return "q1";
-        case "q1": return "q1";
-        case "q2": return "q1";
-        default: return "q0";
+const δ = (s, Σ) => {
+    if (Σ === '0') {
+        switch (s) {
+            case "q0": return "q1";
+            case "q1": return "q1";
+            case "q2": return "q1";
+        }
+    }
+    else {
+        switch (s) {
+            case "q0": return "q0";
+            case "q1": return "q2";
+            case "q2": return "q0";
+        }
     }
     ;
 };
-const one = s => {
-    switch (s) {
-        case "q0": return "q0";
-        case "q1": return "q2";
-        case "q2": return "q0";
-        default: return "q0";
-    }
-    ;
-};
+/*
+const one: δ = (s: State) => {
+  switch (s) {
+    case "q0": return "q0"
+    case "q1": return "q2"
+    case "q2": return "q0"
+    default: return "q0"
+  };
+}
+
+*/
 const run = (dfa, input) => input.reduce((Q, Σ) => dfa.δ(Q, Σ), dfa.S);
 const accepts = (dfa, input) => {
     const finalState = run(dfa, input);
     return dfa.F.has(finalState);
 };
 const dfa = {
-    δ: (zero),
+    δ: δ,
     S: "q0",
     F: new Set(["q1"])
 };
 const dfaTwo = {
-    δ: (one),
+    δ: δ,
     S: "q0",
     F: new Set(["q2"])
 };
-console.log(accepts(dfa, ["0", "1", "0"])); // - this was true.
-console.log(accepts(dfaTwo, ["0", "1", "0", "1", "1", "0", "0"]));
+const languages = [["0", "1", "1", "1", "1", "0", "1"], ["0", "0"], ["0", "1", "1", "1", "1", "0", "1", "0"]];
+const printStatus = (dfa, lang) => {
+    const status = accepts(dfa, lang) ? "Accepted" : "Rejected";
+    console.log(`${lang.join("")}: ${status}`);
+};
+const testDFARestuls = (dfa, languages) => {
+    languages.forEach(lang => printStatus(dfa, lang));
+};
+console.log(testDFARestuls(dfa, languages));
+/*
+console.log(accepts(dfa, ["0", "1", "0"]))
+console.log(accepts(dfaTwo, ["0", "1", "0", "1", "1", "0", "0"]))
+console.log(accepts(dfaTwo, ["1", "0"]))
+console.log(accepts(dfaTwo, ["1", "0"]))
+*/
--- ts/fun/DFA.ts
+++ ts/fun/DFA.ts
@@ -1,23 +1,33 @@
 type State = 'q0' | 'q1' | 'q2'
 type Language = '0' | '1'
+type Statuses = "Accepted" | "Rejected" | "Undecided"
 
 type DFA = {
-  δ: (Q: State, Σ: Language) => State, // δ = Transition function: δ(q, x) = Q × Σ → Q the state reached on reading x
+  δ: (Q: State, Σ: Language) => State, // δ = Transition function: δ(Q, Σ) → Q= Q × Σ → Q the state reached on reading x
   S: State, // S = q0 | Start state
   F: Set<State> // the set of final states
 }
 
 
 type δ = (Q: State, Σ: Language) => State;
-const zero: δ = s => {
-  switch (s) {
-    case "q0": return "q1"
-    case "q1": return "q1"
-    case "q2": return "q1"
-    default: return "q0"
+const δ: δ = (s: State, Σ: Language) => {
+  if (Σ === '0') {
+    switch (s) {
+      case "q0": return "q1"
+      case "q1": return "q1"
+      case "q2": return "q1"
+    }
+  } else {
+    switch (s) {
+      case "q0": return "q0"
+      case "q1": return "q2"
+      case "q2": return "q0"
+    }
   };
 }
-const one: δ = s => {
+
+/*
+const one: δ = (s: State) => {
   switch (s) {
     case "q0": return "q0"
     case "q1": return "q2"
@@ -26,6 +36,7 @@ const one: δ = s => {
   };
 }
 
+*/
 const run = (dfa: DFA, input: Language[]): State => input.reduce((Q, Σ) => dfa.δ(Q, Σ), dfa.S);
 
 const accepts = (dfa: DFA, input: Language[]): boolean => {
@@ -34,18 +45,34 @@ const accepts = (dfa: DFA, input: Language[]): boolean => {
 }
 
 const dfa: DFA = {
-  δ: (zero),
+  δ: δ,
   S: "q0",
   F: new Set(["q1"])
 }
 
 const dfaTwo: DFA = {
-  δ: (one),
+  δ: δ,
   S: "q0",
   F: new Set(["q2"])
 }
 
-console.log(accepts(dfa, ["0", "1", "0"])) // - this was true.
-console.log(accepts(dfaTwo, ["0", "1", "0", "1", "1", "0", "0"]))
+const languages: Language[][] = [["0", "1", "1", "1", "1", "0", "1"], ["0", "0"], ["0", "1", "1", "1", "1", "0", "1", "0"]]
 
+const printStatus = (dfa: DFA, lang: Language[]) => {
+  const status = accepts(dfa, lang) ? "Accepted" : "Rejected"
+  console.log(`${lang.join("")}: ${status}`)
+};
+
+const testDFARestuls = (dfa: DFA, languages: Language[][]): void => {
+  languages.forEach(lang => printStatus(dfa, lang));
+}
+
+console.log(testDFARestuls(dfa, languages));
+
+/*
+console.log(accepts(dfa, ["0", "1", "0"]))
+console.log(accepts(dfaTwo, ["0", "1", "0", "1", "1", "0", "0"]))
+console.log(accepts(dfaTwo, ["1", "0"]))
+console.log(accepts(dfaTwo, ["1", "0"]))
+*/
 
